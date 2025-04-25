89d91ace202ac20671e24ec32b4a2b70cb29b62e
Niru <nirupama.n@acviss.com>
2025-04-25 15:47:11 +0530

    uploading folder

---
+++ src/main/java/OurGITPractice/OurGITPractice/Animal.java
@@ -0,0 +1,14 @@
+package OurGITPractice.OurGITPractice;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("learning git commands");
+		System.out.println("branch");
+		System.out.println("niru");
+		System.out.println("niruagain");
+	}
+
+}
---
+++ src/test/java/OurGITPractice/OurGITPractice/AppTest.java
@@ -0,0 +1,11 @@
+package OurGITPractice.OurGITPractice;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	@Test
+    public static void AppTest()
+    {
+        System.out.println("Learning jenkins");
+    }
+}
